8b0e4b9818a9471296a2364a80178ac4999f43e2
joe.chacon <joe.chacon@ruralsourcing.com>
2017-08-24 16:40:44 -0600

    Created functionality to load player data

--- script.py
+++ script.py
@@ -1,16 +1,50 @@
 import nflgame
-
-year = 2016
-week = 1
-games = nflgame.games(2016, week=1)
-players = nflgame.combine_game_stats(games)
-plays = nflgame.combine_plays(games)
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
 
 
-print ("\n------ RUSHING YARDS------\n")
-for p in players.rushing().sort('rushing_yds').limit(10):
-    msg = '%s %d carries for %d yards and %d TDs'
-    print( msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
-print ("\n------ PASSING YARDS------\n")
-for p in plays.sort('passing_yds').limit(10):
-    print p
+def loadGame(year):
+    games = nflgame.games(year)
+    print("Loaded {} games.".format(year))
+    return games
+
+def getPlayers(game):
+    players = nflgame.combine_game_stats(game)
+    print("Loaded all player data.")
+    return players
+
+def getRushing(players):
+    runs = pd.Series([p.rushing_yds for p in players.rushing()])
+    print("Rushing Data: {}".format(runs))
+    return runs
+
+
+def plot(players):
+    print("Plotting data")
+    fig = plt.figure(figsize=(12,8))
+
+    ax = fig.add_subplot(111)
+    ax.set_title('age vs. rushing yds', fontsize=14, color='green')
+    fig.subplots_adjust(top=0.85)
+
+    ax.set_ylabel('age(days)')
+    ax.set_xlabel('rushing_yds')
+
+    plt.style.use('ggplot')
+
+    for p in players.rushing():
+        plt.scatter(p.rushing_yds, (datetime.datetime.today() - datetime.datetime.strptime(p.player.birthdate,'%m/%d/%Y')).days)
+
+def main():
+    print("Program Started....")
+    games = loadGame(2016)
+    players = getPlayers(games)
+    runs = getRushing(players)
+    plot(players)
+    return
+
+
+
+main()
+
